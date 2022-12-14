edf2e08965e7a83ebe1eda6de84290704ab5722a
AleexxG <alexanderg.sln@gmail.com>
2022-12-14 23:18:58 +0100

    Display data

--- app.js
+++ app.js
@@ -9,20 +9,35 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    const sendData = {location: "None", temp: "N/A", disc: "None", press: "N/A", wind: "N/A", humid: "N/A"}
+    res.render("index", {sendData: sendData});
 });
 
 app.post("/", async (req, res) => {
-    let location =  await req.body.city;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=metric`;
-    const response = await fetch(url);
-    const weatherData = await response.json();
-    
-    const temp = Math.floor(weatherData.main.temp);
-    const disc = weatherData.weather[0].description;
-    const press = weatherData.main.pressure;
-    const wind = weatherData.wind.speed;
-    const humid = weatherData.main.humidity;
+    try {
+        let location =  await req.body.city;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=metric`;
+        const response = await fetch(url);
+        const weatherData = await response.json();
+        
+        const temp = Math.floor(weatherData.main.temp);
+        const disc = weatherData.weather[0].description;
+        const press = weatherData.main.pressure;
+        const wind = weatherData.wind.speed;
+        const humid = weatherData.main.humidity;
+
+        const sendData = {};
+        sendData.location = location;
+        sendData.temp = temp;
+        sendData.disc = disc;
+        sendData.press = press;
+        sendData.wind = wind;
+        sendData.humid = humid;
+        res.render("index", {sendData: sendData});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({data: "Not Found!"})
+    }
 });
 
 app.listen(3000, () => {
