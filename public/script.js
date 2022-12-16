79eab4420112e70c556ce3130e60fe115dd8a6d3
AleexxG <alexanderg.sln@gmail.com>
2022-12-16 23:58:50 +0100

    Mobile responsive

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let bgImg = document.querySelector(".body");
+let bgImg = document.querySelector(".bg_img");
 let icon = document.querySelector(".icon").src;
 
 if (icon === "http://openweathermap.org/img/wn/01d@2x.png") {
@@ -7,10 +7,10 @@ if (icon === "http://openweathermap.org/img/wn/01d@2x.png") {
 else if (icon === "http://openweathermap.org/img/wn/01n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/clearSkyN.jpg')"
 }
-else if (icon === "http://openweathermap.org/img/wn/02d@2x.png" || "http://openweathermap.org/img/wn/03d@2x.png") {
+else if (icon === "http://openweathermap.org/img/wn/02d@2x.png" || icon === "http://openweathermap.org/img/wn/03d@2x.png") {
     bgImg.style.backgroundImage = "url('./img/fewCloudsD.jpg')"
 }
-else if (icon === "http://openweathermap.org/img/wn/02n@2x.png" || "http://openweathermap.org/img/wn/03n@2x.png") {
+else if (icon === "http://openweathermap.org/img/wn/02n@2x.png" || icon === "http://openweathermap.org/img/wn/03n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/fewCloudsN.jpg')"
 }
 else if (icon === "http://openweathermap.org/img/wn/04d@2x.png") {
@@ -19,10 +19,10 @@ else if (icon === "http://openweathermap.org/img/wn/04d@2x.png") {
 else if (icon === "http://openweathermap.org/img/wn/04n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/brokenCloudsN.jpg')"
 }
-else if (icon === "http://openweathermap.org/img/wn/09d@2x.png" || "http://openweathermap.org/img/wn/10d@2x.png") {
+else if (icon === "http://openweathermap.org/img/wn/09d@2x.png" || icon === "http://openweathermap.org/img/wn/10d@2x.png") {
     bgImg.style.backgroundImage = "url('./img/rainD.jpg')"
 }
-else if (icon === "http://openweathermap.org/img/wn/09n@2x.png" || "http://openweathermap.org/img/wn/10n@2x.png") {
+else if (icon === "http://openweathermap.org/img/wn/09n@2x.png" || icon === "http://openweathermap.org/img/wn/10n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/rainN.jpg')"
 }
 else if (icon === "http://openweathermap.org/img/wn/11d@2x.png") {
@@ -31,7 +31,7 @@ else if (icon === "http://openweathermap.org/img/wn/11d@2x.png") {
 else if (icon === "http://openweathermap.org/img/wn/11n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/thunderstormN.jpg')"
 }
-else if (icon === "http://openweathermap.org/img/wn/13d@2x.png" || "http://openweathermap.org/img/wn/13n@2x.png") {
+else if (icon === "http://openweathermap.org/img/wn/13d@2x.png" || icon === "http://openweathermap.org/img/wn/13n@2x.png") {
     bgImg.style.backgroundImage = "url('./img/snow.jpg')"
 }
 else if (icon === "http://openweathermap.org/img/wn/50d@2x.png") {
